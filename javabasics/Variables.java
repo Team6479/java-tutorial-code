d5af8d4032dadf289a68833bdb08daa2c7ec27e6
Tishya Chhabra <tatooine888@gmail.com>
2020-06-07 22:48:45 -0700

    created variables

---
+++ javabasics/Variables.java
@@ -0,0 +1,17 @@
+package javabasics;
+
+public class Variables{
+
+  public static void main(String[] args){
+    int age = 15;
+    boolean isSenior = false;
+    double gpa = 9.0;
+    char lastInitial = 'C';
+    
+    String name = "Tishya";
+    
+    System.out.println(age);
+    //should print out 15
+  }
+
+}
